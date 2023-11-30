c7411fcf9198c0b90b54c5c1a801ebf0f4bd8b7c
johnathanbryce <johnathanbryce@rompathlete.com>
2023-11-30 14:29:25 -0800

    setup content containers and the line content separator colours

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -5,11 +5,13 @@ import Link from 'next/link'
 
 export default function Intro() {
   return (
-    <section>
+    <section className={styles.intro}>
         <h1> Hello </h1>
-        <p> My name is Johnathan Bryce. I am a Vancouver-based Front-End Web Developer proficient in JavaScript, TypeScript, React, Next. js, CSS, and HTML </p>
-        <p> I am passionate about crafting dynamic, engaging websites and applications, constantly fueling my drive for continuous learning and staying up-to-date with industry trends.</p>
-        <p> For web development inquiries, email me at <Link href="mailto:johnathanbryce@gmail.com" target='_blank' > johnathanbryce@gmail.com.</Link> </p>
+        <span className={styles.content_wrapper}>
+          <p> My name is Johnathan Bryce. I am a Vancouver-based Front-End Web Developer proficient in JavaScript, TypeScript, React, Next. js, CSS, and HTML </p>
+          <p> I am passionate about crafting dynamic, engaging websites and applications, constantly fueling my drive for continuous learning and staying up-to-date with industry trends.</p>
+          <p> For web development inquiries, email me at <Link href="mailto:johnathanbryce@gmail.com" target='_blank' > johnathanbryce@gmail.com.</Link> </p>
+        </span>
     </section>
   )
 }
