58d7e945814936d15b322249fcc0f81d34a43a0d
johnathanbryce <johnathanbryce@gmail.com>
2024-10-30 15:12:55 -0700

    feat: added quote quest and mcq markets to portfolio; minor ui updates to clicking the containers

--- src/components/Cards/CategoryCard/CategoryCard.tsx
+++ src/components/Cards/CategoryCard/CategoryCard.tsx
@@ -1,59 +1,67 @@
-'use client'
-import React, {useState, useEffect} from 'react'
-import styles from './CategoryCard.module.css';
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./CategoryCard.module.css";
 
 interface CategoryCardProps {
-    title: string,
-    contentList: string[],
-    setActiveContent: (content: string | null) => void;
-    setActiveCategory?: () => void,
-    isActive: boolean;
-    activeContent: string | null;
+  title: string;
+  contentList: string[];
+  setActiveContent: (content: string | null) => void;
+  setActiveCategory?: () => void;
+  isActive: boolean;
+  activeContent: string | null;
 }
 
-export default function CategoryCard({title, contentList, setActiveContent, setActiveCategory, activeContent, isActive}: CategoryCardProps) {
-    const [activeSection, setActiveSection] = useState<string | null>(null);
+export default function CategoryCard({
+  title,
+  contentList,
+  setActiveContent,
+  setActiveCategory,
+  activeContent,
+  isActive,
+}: CategoryCardProps) {
+  const [activeSection, setActiveSection] = useState<string | null>(null);
 
-    console.log(activeSection, 'card')
+  // ensures that on the secondary click of activeSection, 'Intro' content is displayed
+  const handleItemClick = (clickedSection: string) => {
+    setActiveContent(clickedSection);
+    setActiveSection(clickedSection);
+  };
 
-    // ensures that on the secondary click of activeSection, 'Intro' content is displayed
-    const handleItemClick = (clickedSection: string) => {
-        setActiveContent(clickedSection);
-        setActiveSection(clickedSection);
-    };
+  // removes .active_link class when a different CategoryCard is selected
+  useEffect(() => {
+    if (!isActive) {
+      setActiveSection(null);
+    }
+  }, [isActive]);
 
-    // removes .active_link class when a different CategoryCard is selected
-    useEffect(() => {
-      if (!isActive) {
-        setActiveSection(null);
-      }
-    }, [isActive]);
+  // synchronize activeSection with activeContent from context
+  useEffect(() => {
+    if (activeContent && !contentList.includes(activeContent)) {
+      setActiveSection(null);
+    } else {
+      setActiveSection(activeContent);
+    }
+  }, [activeContent, contentList]);
 
-    // synchronize activeSection with activeContent from context
-    useEffect(() => {
-        if (activeContent && !contentList.includes(activeContent)) {
-            setActiveSection(null);
-        } else {
-            setActiveSection(activeContent);
-        }
-        }, [activeContent, contentList]);
- 
-    return (
-    <div className={`${isActive ? styles.active : ''} ${styles.category_card}`}>
-        <h2 className={styles.title} onClick={setActiveCategory}>{title}</h2>
-        <ul className={styles.list_flex_wrapper}>
-            {contentList.map((content: any, index: number) => (
-            <li 
-                key={index} 
-                className={`${styles.link} ${activeSection === content ? styles.active_link : ''}`} 
-                onClick={() => {handleItemClick(content)
-            }}>
-                <div className={styles.link_text}>{content}</div>
-            </li>
-            ))}
-        </ul>
+  return (
+    <div
+      className={`${isActive ? styles.active : ""} ${styles.category_card}`}
+      onClick={setActiveCategory}
+    >
+      <h2 className={styles.title}>{title}</h2>
+      <ul className={styles.list_flex_wrapper}>
+        {contentList.map((content: any, index: number) => (
+          <li
+            key={index}
+            className={`${styles.link} ${activeSection === content ? styles.active_link : ""}`}
+            onClick={() => {
+              handleItemClick(content);
+            }}
+          >
+            <div className={styles.link_text}>{content}</div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
-
-         
